1c72195f7bd57a7b2251f7e2254e818e037a8c0f
jasonngt83 <jasonngt83@gmail.com>
2025-04-15 22:01:35 -0700

    another change to about page picture note

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ const About = () => (
     <section class="background">
       <div className="about">
       <h2>About Me</h2>
-      <img src="../src/assets/images/avatar.jpg" alt="Jason Nguyen" className="about-avatar" />
+      <img src="../src/assets/images/avatar.jpg" alt="this is where my pic is, it showed ran npm run start command" className="about-avatar" />
       <p>Detail-oriented and innovative Software Engineer with a strong foundation in developing scalable web applications and robust backend solutions. Proficient in multiple programming languages, frameworks, and cloud technologies. Adept at collaborating with cross-functional teams to drive efficiency, optimize software performance, and deliver high-quality solutions. Passionate about problem-solving and continuous learning in the ever-evolving tech landscape.</p>
       </div>
     </section>
